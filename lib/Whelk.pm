876774864607708a3188a2345f3ec48cf86d6e0d
bbrtj <bbrtj.pro@gmail.com>
2024-07-03 10:15:53 +0200

    Add important documentation
    some documentation still missing

--- lib/Whelk.pm
+++ lib/Whelk.pm
@@ -25,9 +25,40 @@ Whelk - A friendly API framework based on Kelp
 
 =head1 SYNOPSIS
 
-	TODO
+	$ kelp-generator --type=whelk MyResource
+	$ plackup
 
 =head1 DESCRIPTION
 
-TODO
+Whelk is an API framework which helps you create correct, fast,
+self-documenting APIs. It's easy to learn, fun to work with and extremely
+customizable. It can be run either standalone as a L<Plack> application, or
+inside a L<Kelp> application as a module.
+
+Whelk is integrated with OpenAPI/Swagger and automatically generates a document
+according to spec v3 rules. All the data for the document is taken directly
+from your endpoint validation rules, which ensures only minimal effort is
+needed to generate a documentation for your project, which can be beautifully
+visualized using OpenAPI tools like Swagger UI.
+
+To get started, take a look at L<Whelk::Manual>.
+
+=head1 ACKNOWLEDGEMENTS
+
+This module was inspired by L<Raisin>.
+
+Thank you to Stefan Geneshky who created L<Kelp>.
+
+=head1 AUTHOR
+
+Bartosz Jarzyna E<lt>bbrtj.pro@gmail.comE<gt>
+
+Consider supporting my effort: L<https://bbrtj.eu/support>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2024 by Bartosz Jarzyna
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
